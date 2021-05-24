510c5f217f36307fe03339cd20e294411889cf2c
Sheogarath <EjJKp3VnBETsvg9>
2021-05-24 23:16:24 +0300

    Authorization added

--- src/CommandExecutor.java
+++ src/CommandExecutor.java
@@ -21,6 +21,7 @@ public class CommandExecutor {
     public void execute(ObjectInputStream inputStream, DataOutputStream outputStream) throws ClassNotFoundException, ParserConfigurationException {
         users.put("a", "a");
         users.put("b", "b");
+
         // принимаем сообщение
         boolean endOfStream = false;
         while (!endOfStream) {
